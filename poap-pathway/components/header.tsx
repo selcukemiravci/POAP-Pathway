bee3ce5b538dd5ba0a4ce176fe8e87559a27d8cc
Emir Avci <emiravc@gmail.com>
2023-10-09 23:58:53 -0600

    Update icon

--- poap-pathway/components/header.tsx
+++ poap-pathway/components/header.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css';
 export default function Header() {
     return(
         <div className={styles.header}>
-            <Image src={"/Thirdweb-Icon-Circle-Black-08 1.png"} alt="" height={48} width={48}/>
+            <Image src={"/app-icon.png"} alt="" height={48} width={48}/>
             <p>Thirdweb PWA Template</p>
         </div>
     )
