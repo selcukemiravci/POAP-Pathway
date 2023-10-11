e37bd96653b19b3d733da8680f826f66130894f1
Emir Avci <emiravc@gmail.com>
2023-10-11 01:09:29 -0600

    Prod without XRPL

--- next.config.js
+++ next.config.js
@@ -7,7 +7,8 @@ const withPWA = require("next-pwa")({
 });
 
 module.exports = withPWA({
-  basePath: "",
+  basePath: '/POAP-Pathway',
+  assetPrefix: '/POAP-Pathway/',  
   reactStrictMode: true,
   images: { unoptimized: true },
 });
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,32 @@
 import type { AppProps } from "next/app";
 import {
   ThirdwebProvider,
-  paperWallet,
-  smartWallet,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+  localWallet,
+  rainbowWallet,
+  embeddedWallet,
 } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import Navbar from "../components/navbar";
 import Header from "../components/header";
 
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "goerli";
-const walletConfig = paperWallet({
-  paperClientId: process.env.NEXT_PUBLIC_PAPER_CLIENT_ID
-});
-
-const smartWalletConfig = smartWallet(walletConfig, {
-  factoryAddress: "0xc182827dF66A1fD9ADd9a39130D8dd6074F8F610",
-  gasless: true
-});
-
+const activeChain = "goerli";  // You can keep this or change to "mumbai" based on your preference
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
       activeChain={activeChain}
-      supportedWallets={[smartWalletConfig]}
+      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+      supportedWallets={[
+        metamaskWallet({ recommended: true }),
+        coinbaseWallet(),
+        walletConnect(),
+        localWallet(),
+        embeddedWallet({ recommended: true }),
+        rainbowWallet(),
+      ]}
     >
       <Header />
       <Component {...pageProps} />
@@ -35,6 +34,5 @@ function MyApp({ Component, pageProps }: AppProps) {
     </ThirdwebProvider>
   );
 }
-
 
 export default MyApp;
