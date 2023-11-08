ed3b890b47145d52baa72e4f5daae5228002ad04
Emir Avci <selcuk.avci@ucalgary.ca>
2023-11-07 23:44:15 -0700

    goerli intergration for Production

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,25 +16,7 @@ import Header from "../components/header";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-    activeChain={{
-      // === Required information for connecting to the network === \\
-      chainId: 1440002, // Chain ID of the network
-      // Array of RPC URLs to use
-      rpc: ["rpc-evm-sidechain.xrpl.org"],
-
-      // === Information for adding the network to your wallet (how it will appear for first time users) === \\
-      // Information about the chain's native currency (i.e. the currency that is used to pay for gas)
-      nativeCurrency: {
-        decimals: 18,
-        name: "XRPL EVM Sidechain",
-        symbol: "xrpl",
-      },
-      shortName: "xprlevm", // Display value shown in the wallet UI
-      slug: "xprlevm", // Display value shown in the wallet UI
-      testnet: true, // Boolean indicating whether the chain is a testnet or mainnet
-      chain: "XRPL EVM Sidechain", // Name of the network
-      name: "XRPL EVM Sidechain Testnet", // Name of the network
-    }}
+    activeChain="goerli"
       clientId= "79e84b40b40d9bfb8d979db500f698b8"
       supportedWallets={[
         metamaskWallet({ recommended: true }),
